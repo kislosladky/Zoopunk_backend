773ed098702564f18fabdb6bfd504b77928c3413
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2023-10-10 21:49:09 +0700

    project init

---
+++ backend/src/main/java/zoopunk/backend/ZoopunkBackendApplication.java
@@ -0,0 +1,13 @@
+package zoopunk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoopunkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoopunkBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/zoopunk/backend/ZoopunkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zoopunk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoopunkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
