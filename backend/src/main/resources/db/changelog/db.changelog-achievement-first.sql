e04946ae9271bc8cfae93fbbbd277f5897eb626e
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-03-26 11:28:53 +0700

    fixing typo in achievements

--- backend/src/main/resources/db/changelog/db.changelog-achievement-first.sql
+++ backend/src/main/resources/db/changelog/db.changelog-achievement-first.sql
@@ -1,29 +1,29 @@
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c60', 'Знаток зоопарка', 'История зоопарка очень интересна, давай пройдём тест!', 15)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c61', 'Юный Орнитолог', 'В России обитает много разных, красивых и интересных птиц, пройди тест и получи очивку!', 10)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c62', 'Опытный Орнитолог', 'В России обитает много разных, красивых и интересных птиц, пройди тест и получи очивку!', 20)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c63', 'Главный знаток птиц', 'В России обитает много разных, красивых и интересных птиц, пройди тест и получи очивку!', 30)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c64', 'Обезьянье обаяние', 'Пройди тест по самым обаятельным животным зоопарка - обезьянкам!', 10)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c65', 'Животный детектив', 'Прогуляйся по зоопарку, узнай много нового о животных и ответь на вопросы в тесте!', 10)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c66', 'Крокодилья сила', 'В чём сила, брат?', 10)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c667', 'Подводный мир', 'Подводный мир очень разнообразен и интересен, пройди тест и получи очивку!', 10)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c668', 'Знаток подводного мира', 'Ты хорошо знаешь подводный мир!', 15)
 
-INSERT INTO achivements (id, title, description, pointsfordone)
+INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c668', 'Рыба в воде', 'Ты отлично знаешь подводный мир, ты как рыба в воде!', 30)
