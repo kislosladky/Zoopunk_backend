da2a52b8e4879621156b2516e5f74769f9624407
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-03-26 11:44:32 +0700

    fixed uuids

--- backend/src/main/resources/db/changelog/db.changelog-achievement-first.sql
+++ backend/src/main/resources/db/changelog/db.changelog-achievement-first.sql
@@ -17,13 +17,13 @@ INSERT INTO achievements (id, title, description, pointsfordone)
     VALUES ('85557a87-6908-41af-ad16-e8f959871c65', 'Животный детектив', 'Прогуляйся по зоопарку, узнай много нового о животных и ответь на вопросы в тесте!', 10);
 
 INSERT INTO achievements (id, title, description, pointsfordone)
-    VALUES ('85557a87-6908-41af-ad16-e8f959871c66', 'Крокодилья сила', 'В чём сила, брат?', 10);
+    VALUES ('85557a87-6908-41af-ad16-e8f959c71c66', 'Крокодилья сила', 'В чём сила, брат?', 10);
 
 INSERT INTO achievements (id, title, description, pointsfordone)
-    VALUES ('85557a87-6908-41af-ad16-e8f959871c667', 'Подводный мир', 'Подводный мир очень разнообразен и интересен, пройди тест и получи очивку!', 10);
+    VALUES ('85557a87-6908-41af-ad16-e8fb59871c67', 'Подводный мир', 'Подводный мир очень разнообразен и интересен, пройди тест и получи очивку!', 10);
 
 INSERT INTO achievements (id, title, description, pointsfordone)
-    VALUES ('85557a87-6908-41af-ad16-e8f959871c668', 'Знаток подводного мира', 'Ты хорошо знаешь подводный мир!', 15);
+    VALUES ('85557a87-6908-41af-ad16-e8a959871c68', 'Знаток подводного мира', 'Ты хорошо знаешь подводный мир!', 15);
 
 INSERT INTO achievements (id, title, description, pointsfordone)
-    VALUES ('85557a87-6908-41af-ad16-e8f959871c668', 'Рыба в воде', 'Ты отлично знаешь подводный мир, ты как рыба в воде!', 30);
+    VALUES ('85557a87-6908-41af-ad16-e8f95a871c69', 'Рыба в воде', 'Ты отлично знаешь подводный мир, ты как рыба в воде!', 30);
