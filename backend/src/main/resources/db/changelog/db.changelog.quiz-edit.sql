c5cb15797ef9c1b15f0082d04663c013fb0a1872
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-04-01 21:17:17 +0700

    dropping old quizzes

--- backend/src/main/resources/db/changelog/db.changelog.quiz-edit.sql
+++ backend/src/main/resources/db/changelog/db.changelog.quiz-edit.sql
@@ -1,7 +1,12 @@
-ALTER TABLE quizzes
-    ADD COLUMN name TEXT,
-    ADD COLUMN img TEXT,
-    ADD COLUMN description TEXT;
+DROP TABLE IF EXISTS quizzes;
+CREATE TABLE quizzes {
+    id                  UUID PRIMARY KEY NOT NULL,
+    achievementsid      UUID,
+    name                TEXT,
+    img                 TEXT,
+    description         TEXT,
+    quizcontent         JSON
+};
 
 INSERT INTO quizzes (id, achievementsid, name, img, description, quizcontent) VALUES
      ('9ff4d17f-d885-44dc-8b00-1ed57b611386', '85557a87-6908-41af-ad16-e8f959c71c66', 'Тестовый квиз', '../../assets/blueBulp.png', 'Очень интересный квиз для детей',  '{ "count-questions": 5, "results":[{"question":"Как называют детёныша курицы?", "correct_answer":"Цыплёнок","incorrect_answers":["Курёнок","Курица-грилль","KFC"]},{"question":"Сколько ног у осьминога","correct_answer":"8","incorrect_answers":["Ни одной","Урааа, суши","25"]}, {"question":"Кто самое медлительное животное?","correct_answer":"Ленивец","incorrect_answers":["Программист с почасовой оплатой","Подкованная блоха","Птица говорун"]}, {"question":"Кто из животных носит сумку?", "correct_answer":"Кенгуру",   "incorrect_answers":["Крокодилиха","Светская львица"]},{"question":"Самое большое хищное животное", "correct_answer":"Белый медведь","incorrect_answers":["Волк с Уолл-Стрит","Иртегов","Колибри"]}]}'),
