b5d11ba8dbaea19cacab313c9ea9fde1386ee653
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-04-03 11:52:49 +0700

    edited users table

--- backend/src/main/resources/db/changelog/db.changelog-authorization.sql
+++ backend/src/main/resources/db/changelog/db.changelog-authorization.sql
@@ -1,2 +1,5 @@
 ALTER TABLE users
-    RENAME COLUMN nickname TO username;
+    RENAME COLUMN nickname TO username,
+    ADD COLUMN role TEXT,
+    ADD COLUMN password TEXT,
+    ADD COLUMN email TEXT;
