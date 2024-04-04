b553655db7d7f08fdc55c201e62444f4a7316e36
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-04-04 18:12:06 +0700

    forgot id annotaion in route entity

--- backend/src/main/java/zoopunk/backend/Entity/Route.java
+++ backend/src/main/java/zoopunk/backend/Entity/Route.java
@@ -2,6 +2,7 @@ package zoopunk.backend.Entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Data
 @Entity(name="routes")
 public class Route {
+    @Id
     @Column(name="id")
     private UUID id;
 
