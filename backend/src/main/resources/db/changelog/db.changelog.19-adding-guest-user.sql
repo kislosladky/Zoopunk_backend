57b6f346157ffc675f005a1b9f1184758470e3d0
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-04-26 20:13:11 +0700

    fixing guest sign in

--- backend/src/main/resources/db/changelog/db.changelog.19-adding-guest-user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id, password, email, role, firstname, username, image)
-    VALUES ('083e404f-1d03-4b42-8b54-1c5b7225558a', '11111111', 'guest@gmail.com', 'ROLE_GUEST', 'гость', 'guest', 'parrotAva.png');
